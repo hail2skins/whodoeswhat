d933c02dec3c7f784a90336ed4f4840e04d23b30
Art Mills <art@hamcois.com>
2016-02-17 02:46:18 +0000

    Cleaning up tests.

--- test/integration/visit_welcome_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-class VisitWelcomeTest < ActionDispatch::IntegrationTest
-  test "visit root path" do
-    visit root_path
-    assert page.has_content?(Time.now), "Time.now not here baby."
-  end
-end
