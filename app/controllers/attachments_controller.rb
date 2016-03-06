37fe4bd4723cc01c866a7585ef3b428eae294136
Art Mills <art@hamcois.com>
2016-03-06 05:47:12 +0000

    See AWS files

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -3,7 +3,7 @@ class AttachmentsController < ApplicationController
   def show
     attachment = Attachment.find(params[:id])
     authorize attachment
-    send_file attachment.file.path, disposition: :inline
+    send_file file_to_send(attachment), disposition: :inline
   end
   
   def new
@@ -12,6 +12,19 @@ class AttachmentsController < ApplicationController
     @article.attachments.build
     render layout: false
   end
-  
+
+  private
+
+  def file_to_send(attachment)
+    if URI.parse(attachment.file.url).scheme
+      filename = "/tmp/#{attachment.attributes["file"]}"
+      File.open(filename, "wb+") do |tf|
+        tf.write open(attachment.file.url).read
+      end
+      filename
+    else
+      attachment.file.path
+    end
+  end  
   
 end
