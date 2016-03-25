251873f7b423e4c195d2f558cd719ef19fe35fcc
Art Mills <art@hamcois.com>
2016-03-25 14:34:46 +0000

    Just doing to do, but need to work tonight.

--- test/integration/update_articles_test.rb
+++ test/integration/update_articles_test.rb
@@ -97,7 +97,6 @@ class UpdateArticlesTest < ActionDispatch::IntegrationTest
   test "update article without contacts with one contact" do
     visit edit_business_article_path(article_business, article_one)
     
-    
   end
   
 end
