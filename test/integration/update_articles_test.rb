237466ccefb650efbbc1a7a7a04894e79c95f668
Art Mills <art@hamcois.com>
2016-04-08 00:19:31 +0000

    Fixing update now and should work.

--- test/integration/update_articles_test.rb
+++ test/integration/update_articles_test.rb
@@ -116,12 +116,12 @@ class UpdateArticlesTest < ActionDispatch::IntegrationTest
     login_as(article_user)
 
     visit edit_business_article_path(article_business, article_one)
-    fill_token_input 'article_contact_tokens', with: "art@test.com Art Mills"
+    fill_token_input 'article_contact_tokens', with: "art1@test.com Art Mills"
     click_button "Update article"
     
     within(".contacts") do
       check_content "Art Mills",
-                    "art@test.com"
+                    "art1@test.com"
                     
     end    
   end
